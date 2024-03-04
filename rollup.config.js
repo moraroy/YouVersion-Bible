a124252419e9ae786b71746ce8d44f143a33aa42
Roy <88516395+moraroy@users.noreply.github.com>
2024-03-04 14:03:52 +0000

    major update 4

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import replace from '@rollup/plugin-replace';
 import typescript from 'rollup-plugin-typescript2';
 import { defineConfig } from 'rollup';
 import importAssets from 'rollup-plugin-import-assets';
+import polyfillNode from 'rollup-plugin-polyfill-node';
 
 import { name } from "./plugin.json";
 
@@ -13,6 +14,7 @@ export default defineConfig({
   plugins: [
     commonjs(),
     json(),
+    polyfillNode(),
     typescript({
       include: ['src/**/*.ts', 'src/**/*.tsx', 'node_modules/@glowstudent/youversion/**/*.ts'],
       exclude: ['node_modules/!(glowstudent)/**/*.ts']
